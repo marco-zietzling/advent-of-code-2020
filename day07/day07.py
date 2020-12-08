bd03859c976e27e82820244fc7609155a5e321ad
Marco Zietzling <marco.zietzling@gmail.com>
2020-12-08 23:06:53 +0100

    day 7 part 2

--- day07/day07.py
+++ day07/day07.py
@@ -3,66 +3,83 @@ import re
 print("advent of code 2020 - day 7")
 
 
-def extract_number_of_bags(bag: str):
-    matched_numbers = re.findall("[0-9]+", bag)
-    bag = re.sub("bags", "", bag)
-    bag = re.sub("bag", "", bag)
-    bag = re.sub("no other", "", bag)
-    bag = re.sub("[0-9]+", "", bag)
-    bag = bag.strip()
+def extract_number_of_bags(_bag: str):
+    _matched_numbers = re.findall("[0-9]+", _bag)
+    _bag = re.sub("bags", "", _bag)
+    _bag = re.sub("bag", "", _bag)
+    _bag = re.sub("no other", "", _bag)
+    _bag = re.sub("[0-9]+", "", _bag)
+    _bag = _bag.strip()
 
-    number = 0
-    if matched_numbers:
-        number = matched_numbers[0]
+    _number = 0
+    if _matched_numbers:
+        _number = _matched_numbers[0]
 
-    # print(f"result is ({number}, {bag})")
-    return number, bag
+    # print(f"result is ({_number}, {_bag})")
+    return _number, _bag
 
 
 def load_bag_rules():
-    result = dict()
+    _result = dict()
 
-    with open("input.txt") as file:
-        for line in file:
-            (container_bag, contained_bags) = line.strip().split(" contain ")
-            (_, cr_bag_name) = extract_number_of_bags(container_bag)
+    with open("input.txt") as _file:
+        for _line in _file:
+            (_container_bag, _contained_bags) = _line.strip().split(" contain ")
+            (_, _cr_bag_name) = extract_number_of_bags(_container_bag)
 
-            contained_bags = contained_bags.strip(".")
-            contained_bag_entries = contained_bags.split(", ")
-            clean_contained_bag_entries = []
+            _contained_bags = _contained_bags.strip(".")
+            _contained_bag_entries = _contained_bags.split(", ")
+            _clean_contained_bag_entries = []
 
-            for entry in contained_bag_entries:
-                (cd_bag_number, cd_bag_name) = extract_number_of_bags(entry)
-                if cd_bag_name != "":
-                    clean_contained_bag_entries.append((cd_bag_number, cd_bag_name))
+            for _entry in _contained_bag_entries:
+                (_cd_bag_number, _cd_bag_name) = extract_number_of_bags(_entry)
+                if _cd_bag_name != "":
+                    _clean_contained_bag_entries.append((_cd_bag_number, _cd_bag_name))
 
-            result[cr_bag_name] = clean_contained_bag_entries
+            _result[_cr_bag_name] = _clean_contained_bag_entries
 
-    # print(result)
-    return result
+    # print(_result)
+    return _result
 
 
 def calculate_part_1():
-    bag_queue = ["shiny gold"]
-    result_bag_set = set()
-    bag_ruleset = load_bag_rules()
+    _bag_queue = ["shiny gold"]
+    _result_bag_set = set()
+    _bag_ruleset = load_bag_rules()
 
-    while bag_queue:
-        # print(f"current queue: {bag_queue}")
-        current_bag = bag_queue[0]
-        bag_queue.remove(current_bag)
-        # print(f"current bag: {current_bag}")
+    while _bag_queue:
+        # print(f"current queue: {_bag_queue}")
+        _current_bag = _bag_queue[0]
+        _bag_queue.remove(_current_bag)
+        # print(f"current bag: {_current_bag}")
 
-        for rule in bag_ruleset:
-            # print(f"current rule: '{rule}' ->  {bag_ruleset[rule]}")
-            if current_bag in list(map(lambda x: x[1], bag_ruleset[rule])):
-                if rule not in result_bag_set:
-                    bag_queue.append(rule)
-                    result_bag_set.add(rule)
+        for _rule in _bag_ruleset:
+            # print(f"current rule: '{_rule}' ->  {_bag_ruleset[_rule]}")
+            if _current_bag in list(map(lambda x: x[1], _bag_ruleset[_rule])):
+                if _rule not in _result_bag_set:
+                    _bag_queue.append(_rule)
+                    _result_bag_set.add(_rule)
 
-    print(f"{result_bag_set}")
-    return len(result_bag_set)
+    print(f"{_result_bag_set}")
+    return len(_result_bag_set)
+
+
+def calculate_part_2(_bag_ruleset, _bag="shiny gold"):
+    _result = 1
+    _contained_bags = _bag_ruleset[_bag]
+    # print(f"current contained bags: {_contained_bags}")
+
+    for (_amount, _contained_bag) in _contained_bags:
+        # print(f"amount={_amount} of bags={_contained_bag}")
+        _result += int(_amount) * calculate_part_2(_bag_ruleset, _contained_bag)
+
+    return _result
 
 
 # result = 335
 print(f"part 1: {calculate_part_1()} bags can contain directly or indirectly the shiny gold bag")
+
+bag_ruleset = load_bag_rules()
+
+# result = 2431 (in order to cater for the shiny gold bag itself, we need to substract -1 from the final result
+print(f"part 2: {calculate_part_2(bag_ruleset) - 1} other bags are contained within a shiny gold bag")
